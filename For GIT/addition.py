8d8747091b3a2d679f7fcb00ffb033ba136919da
Atharv Bhasme <94709794+CodeHub5199@users.noreply.github.com>
2023-07-07 16:40:56 +0530

    Add files via upload

---
+++ For GIT/addition.py
@@ -0,0 +1,24 @@
+input_req = {'text': 2}
+
+import argparse
+
+# Create an argument parser
+parser = argparse.ArgumentParser(description='Perform addition of two numbers.')
+
+# Add command-line arguments
+parser.add_argument('arg1', type=int, help='First number')
+parser.add_argument('arg2', type=int, help='Second number')
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
+input1 = args.arg1
+input2 = args.arg2
+
+def addition(input1, input2):
+    result = input1+input2
+    return result
+
+print(addition(input1, input2))
+
+
